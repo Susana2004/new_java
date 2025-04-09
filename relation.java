899a13fd1971e345638fe063684fddea540d1777
Susana2004 <gs6986164@gmail.com>
2025-04-09 13:13:50 +0530

    save it

---
+++ relation.java
@@ -0,0 +1,15 @@
+package project2;
+
+public class relation {
+    public static void main(String[]args){
+        int a=10;
+        int b=15;
+        System.out.println("a>b="+(a>b));
+        System.out.println("a<b="+(a<b));
+        System.out.println("a==b="+(a==b));
+        System.out.println("a!=b="+(a!=b));
+        System.out.println("a>=b="+(a>=b));
+        System.out.println("a<=b="+(a<=b));
+        }
+    
+}
